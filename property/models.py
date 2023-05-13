44572ebc2eb7404317ccdac0f2e993602c96aac8
igorkorobko <nekto.korobko@gmail.com>
2023-05-14 02:06:54 +0300

    Migrate flats id from flats to owner

--- property/models.py
+++ property/models.py
@@ -67,8 +67,8 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    name = models.CharField('ФИО владельца', max_length=200)
-    phonenumber = models.CharField('Номер владельца', max_length=20)
+    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
     pure_phone = PhoneNumberField('Нормализованный номер владельца', region='RU', null=True, blank=True,
                                         db_index=True)
     flats = models.ManyToManyField(Flat,
